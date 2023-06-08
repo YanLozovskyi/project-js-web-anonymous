d59cd0cfabf6c0aa758a7ee10ba0296f435d8cce
SirotovAlexander <alexander.sirotov@gmail.com>
2023-06-08 19:57:33 +0300

    make-function-for-card
    make-function-for-card

--- src/js/weekly-trends.js
+++ src/js/weekly-trends.js
@@ -14,30 +14,40 @@ async function getTrendMovieOfWeek() {
       return DetailedInformation;
     });
     const moviCard = await Promise.allSettled(infoFromCorr);
-    const createCardMarkup = moviCard.map(item => console.log(item));
+    const createCardMarkup = moviCard
+      .map(item => {
+        const {
+          backdrop_path,
+          genres,
+          original_title,
+          release_date,
+          vote_average,
+        } = item.value.data;
+
+        const markup = `<li class="weekly-trends-item">
+        <a href="#" class="weekly-trends-imgLink"
+          ><img src="https://image.tmdb.org/t/p/original/${backdrop_path}" alt="${original_title}" class="weekly-trends-img"
+        /></a>
+        <div class="weekly-trends-textWrapper">
+          <h2 class="weekly-trends-filmName">${original_title}</h2>
+          <p class="weekly-trends-text">${genres[1].name}|${release_date.slice(
+          0,
+          4
+        )}</p>
+          <span class="weekly-trends-rating">${Math.ceil(vote_average)}</span>
+        </div>
+      </li>`;
+
+        return markup;
+      })
+      .join('');
+
     console.log(createCardMarkup);
 
-    // console.log(infoFromCorr);
-    // const infoGet = await apiMovie.getMovieInfo(569094);
-    // console.log(infoGet);
-    //     console.log('Трендові фільми тижня :', correctList);
-
-    // console.log(createMarkup);
+    list.innerHTML = createCardMarkup;
   } catch (error) {
     console.log('Error:', error);
   }
 }
 
 getTrendMovieOfWeek();
-
-// const markup = `<li class="weekly-trends-item">
-//   <a href="#" class="weekly-trends-imgLink"
-//     ><img src="" alt="" class="weekly-trends-img"
-//   /></a>
-//   <div class="weekly-trends-textWrapper">
-//     <p class="weekly-trends-filmName"></p>
-//     <p class="weekly-trends-genre"></p>
-//     <p class="weekly-trends-date"></p>
-//     <span class="weekly-trends-rating"></span>
-//   </div>
-// </li>`;
