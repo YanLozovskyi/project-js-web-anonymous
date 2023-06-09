1a3ffdbe1ce5c1d6b6585fb79210ff8cb58af253
Sergey4857 <yasak.sergey@gmail.com>
2023-06-09 12:33:30 +0300

    рамка

--- src/catalog.js
+++ src/catalog.js
@@ -1,2 +1,21 @@
 // styles scss
+
 import './sass/main.scss';
+import ApiMovie from './js/api/themoviedbAPI/fetch-movie';
+import { randomElement, createMarkup } from './js/api/themoviedbAPI/hero';
+
+const apiMovie = new ApiMovie();
+
+const catalogPath = document.querySelector('.hero-catalog');
+
+async function getTrendMovieOfDay() {
+  try {
+    const response = await apiMovie.getTrend('day');
+    const randomFilm = randomElement(response.data.results);
+    createMarkup(randomFilm, catalogPath);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+getTrendMovieOfDay();
