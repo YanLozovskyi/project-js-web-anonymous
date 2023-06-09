f8569d2bf4fd7a3926bef35205611505b04791df
UserIryna28 <Ltdcom@ukr.net>
2023-06-09 13:16:22 +0300

    Add changes

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -12,23 +12,24 @@ import ApiMovie from './api/themoviedbAPI/fetch-movie';
 //     return response
 
 const buttonAddLibrary = document.querySelector(".button")
-
-async function getNewFilms() {
-    const apiMovie = new ApiMovie({
-id:569094,
-popularity: 2860.755,
-poster_path: "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-release_date: 2023-05-31,
-title: 'Spider-Man: Across the Spider-Verse',
-vote_average: 8.8,
-vote_count: 925
-});
-    const response = await apiMovie.getNewFilms()
-    console.log(response)
-     const example = await apiMovie.getMovieInfo(569094)
-     console.log(example)
-}
- getNewFilms() 
+const apiMovie = new ApiMovie
+apiMovie.getNewFilms()
+// async function getNewFilms() {
+//     const apiMovie = new ApiMovie({
+// id:"569094",
+// popularity: "2860.755",
+// poster_path: "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+// release_date: "2023-05-31",
+// title: 'Spider-Man: Across the Spider-Verse',
+// vote_average: "8.8",
+// vote_count: "925"
+// });
+//     const response = await apiMovie.getNewFilms()
+//     console.log(response)
+//      const example = await apiMovie.getMovieInfo(569094)
+//      console.log(example)
+// }
+//  getNewFilms() 
 
 
 
@@ -38,34 +39,34 @@ vote_count: 925
 
 //   addEventListener.buttonAddLibrary("click", getMovieInfo)
 
- const movieDescription = document.querySelector(".movieDescription")
+//  const movieDescription = document.querySelector(".movieDescription")
   
- function getNewFilms(movies) {
-    //    const markup = movies
-     return { id, original_title, release_date, vote_average, vote_count, popularity, genre_ids, overview } 
+//  function getNewFilms(movies) {
+//     //    const markup = movies
+//      return { id, original_title, release_date, vote_average, vote_count, popularity, genre_ids, overview } 
            
-        `
-             <a class="gallery__link" href="${backdrop_path}">
-              <div class="gallery-item" id="${ id }">
+//         `
+//              <a class="gallery__link" href="${backdrop_path}">
+//               <div class="gallery-item" id="${ id }">
                 
-                             <div class="info"
-                     <p class="info-item"><b>The lost city</b>${original_title}</p>
-                <p class="info-item"><b>Release date</b>${release_date}</p>
-                 <p class="info-item"><b>Vote</b>${vote_average}
-   </p>
-                   <p class="info-item"><b>Popularity</b>${popularity}</p>
-     < p class="info-item" > <b>Genre</b>${ genre_ids }</p >
-      <p class="info-item"><b>About</b>${overview}</p>
-                 </div>
-               </div>
-            </a> `
-     .join('')
-          movieDescription.innerHTML = getNewFilms(movies)
+//                              <div class="info"
+//                      <p class="info-item"><b>The lost city</b>${original_title}</p>
+//                 <p class="info-item"><b>Release date</b>${release_date}</p>
+//                  <p class="info-item"><b>Vote</b>${vote_average}
+//    </p>
+//                    <p class="info-item"><b>Popularity</b>${popularity}</p>
+//      < p class="info-item" > <b>Genre</b>${ genre_ids }</p >
+//       <p class="info-item"><b>About</b>${overview}</p>
+//                  </div>
+//                </div>
+//             </a> `
+//      .join('')
+//           movieDescription.innerHTML = getNewFilms(movies)
                  
-        }
+//         }
      
     
-    getNewFilms() 
+//     getNewFilms() 
     
 
                    
