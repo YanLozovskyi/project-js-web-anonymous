95ce4b3db3bb550d24c71bcca954584760bdfbb4
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-12 12:54:37 +0300

    add API

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -1,7 +1,6 @@
 import ApiMovie from './api/themoviedbAPI/fetch-movie';
-import Storage from './api/localStorageAPI/localStorageAPI';
-import { randomElement } from './components/randomElement';
-import { STORAGE_KEY } from './localStorageKey/localStorageKey';
+
+import { ServiceAddRemoveBtn } from './api/ServiceAddRemoveBtn/ServiceAddRemoveBtnAPI';
 
 const apiMovie = new ApiMovie();
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
@@ -10,68 +9,28 @@ const movieDescription = document.querySelector('.movieDescription');
 
 getNewFilms();
 
-let toggleAddRemoveButton;
-let movieId = null;
-let randomMovieInfo;
-const dataLocalStorage = Storage.load(STORAGE_KEY.myLibraryMoviesList);
-
 async function getNewFilms() {
   try {
     const response = await apiMovie.getNewFilms();
     const randomFilm = randomElement(response.data.results);
-    const randomFilmId = randomFilm.map(film => film.id).join('');
-    randomMovieInfo = await apiMovie.getMovieInfo(randomFilmId);
-    movieId = randomMovieInfo.data.id;
 
-    movieDescription.innerHTML = createUpcomingMovieMarkup(
-      randomMovieInfo.data
-    );
+    const movie = await apiMovie.getMovieInfo(randomFilm.id);
 
-    toggleAddRemoveButton = document.querySelector('.test');
-    setButtonName();
+    movieDescription.innerHTML = createUpcomingMovieMarkup(movie.data);
+
+    const addRemoveBtn = document.querySelector('.add-remove-btn');
+
+    const serviceAddRemoveBtn = new ServiceAddRemoveBtn(addRemoveBtn, movie);
+
+    serviceAddRemoveBtn.setButtonName();
   } catch (error) {
     console.log(error);
   }
 }
 
-function setButtonName(movieId) {
-  if (dataLocalStorage?.length === 0 || !dataLocalStorage) {
-    Storage.save(STORAGE_KEY.myLibraryMoviesList, []);
-    toggleAddRemoveButton.textContent = 'Add to my library';
-    toggleAddRemoveButton.removeEventListener('click', onAddMovieBtnClick);
-    toggleAddRemoveButton.addEventListener('click', onAddMovieBtnClick);
-  } else {
-    if (dataLocalStorage.some(({ id }) => id === movieId)) {
-      toggleAddRemoveButton.textContent = 'Remove from my library';
-      toggleAddRemoveButton.removeEventListener('click', onRemoveBtnClick);
-      toggleAddRemoveButton.addEventListener('click', onRemoveBtnClick);
-    } else {
-      toggleAddRemoveButton.textContent = 'Add to my library';
-      toggleAddRemoveButton.removeEventListener('click', onAddMovieBtnClick);
-      toggleAddRemoveButton.addEventListener('click', onAddMovieBtnClick);
-    }
-  }
-}
-
-function onRemoveBtnClick() {
-  const localStorageData = Storage.load(STORAGE_KEY.myLibraryMoviesList);
-  const index = localStorageData.findIndex(({ id }) => id === movieId);
-  const updateData = Storage.load(STORAGE_KEY.myLibraryMoviesList);
-  updateData.splice(index, 1);
-
-  Storage.save(STORAGE_KEY.myLibraryMoviesList, updateData);
-  toggleAddRemoveButton.textContent = 'Add to my library';
-  toggleAddRemoveButton.removeEventListener('click', onRemoveBtnClick);
-  toggleAddRemoveButton.addEventListener('click', onAddMovieBtnClick);
-}
-
-function onAddMovieBtnClick() {
-  const localStorageData = Storage.load(STORAGE_KEY.myLibraryMoviesList);
-  const updateData = [...localStorageData, randomMovieInfo.data];
-  Storage.save(STORAGE_KEY.myLibraryMoviesList, updateData);
-  toggleAddRemoveButton.textContent = 'Remove from my library';
-  toggleAddRemoveButton.removeEventListener('click', onAddMovieBtnClick);
-  toggleAddRemoveButton.addEventListener('click', onRemoveBtnClick);
+function randomElement(arr) {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
 }
 
 function createUpcomingMovieMarkup({
@@ -121,6 +80,6 @@ function createUpcomingMovieMarkup({
         >${overview}
       </p>
     </div>
-    <button type="button" class="test button-accent"></button>
+    <button type="button" class="add-remove-btn button-accent"></button>
   </div>`;
 }
