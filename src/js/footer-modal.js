ecddc914b129b993bb7f63a40aa8dd8468722595
Ivano4ka8 <ivannamihalova@gmail.com>
2023-06-12 12:23:56 +0300

    start footer-modal

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,5 +1,23 @@
-const linkCommandModalEl = document.querySelector('.command-modal-link-js');
+const linkCommandModalEl = document.querySelector('.team-modal-link-js');
+const footerModalEl = document.querySelector('.footer-modal-backdrop');
 
 linkCommandModalEl.addEventListener('click', openModalCommand);
 
-function openModalCommand() {}
+function openModalCommand() {
+  document.body.classList.add('no-scroll');
+  footerModalEl.classList.remove('is-hidden');
+
+  footerModalEl.addEventListener('click', closeModal);
+}
+
+function closeModal(e) {
+  if (
+    e.target.closest('.footer-modal-backdrop') &&
+    !e.target.closest('.footer-modal-content')
+  ) {
+    refs.mobileMenu.classList.add('is-hidden');
+    document.body.classList.remove('no-scroll');
+    removeAllEventListeners();
+  }
+  return;
+}
