f488866efa01ea83e1978c9350d982fe0d16dbb8
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-13 19:20:11 +0300

    add  button  watch trailer on modal

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -29,7 +29,7 @@ async function onMovieCardClick(e) {
 
           const addRemoveBtn = instance
             .element()
-            .querySelector('button[data-type="action"]');
+            .querySelector('button[data-action="add-remove-to-my-library"]');
 
           const serviceAddRemoveBtn = new ServiceAddRemoveBtn(
             addRemoveBtn,
@@ -52,15 +52,96 @@ async function onMovieCardClick(e) {
           document.body.style.overflow = '';
           document.body.style.paddingRight = '';
           document.removeEventListener('keydown', this.handlerEscape);
-          document.removeEventListener('click', this.handlerEscape);
+          document.removeEventListener('click', this.handlerBtnClose);
         },
       });
 
       instance.show();
+
+      showTrailer(movie);
     } catch (error) {
       console.log(error);
     }
   }
+}
+
+async function showTrailer(response) {
+  const button = document.querySelector(
+    'button[data-action="add-movie-trailer-btn"]'
+  );
+
+  button.addEventListener('click', onButtonClick);
+
+  async function onButtonClick() {
+    try {
+      const youtubeTrailers = await apiMovie.getTrailer(response.data.id);
+      console.log('youtubeTrailers:', youtubeTrailers);
+
+      const trailer = youtubeTrailers.data.results.find(
+        el => el.type === 'Trailer' || el.name === 'Official Trailer'
+      );
+
+      if (!trailer) {
+        throw new Error('Trailer not found');
+      }
+
+      const instance = basicLightbox.create(
+        `
+       <iframe class="iframe" src="https://www.youtube.com/embed/${trailer.key}" width="560" height="315" frameborder="0"></iframe>`,
+        {
+          handlerEscape: null,
+
+          onShow() {
+            this.handlerEscape = handlerEsc.bind(instance);
+            document.addEventListener('keydown', this.handlerEscape);
+          },
+          onClose() {
+            document.removeEventListener('keydown', this.handlerEscape);
+          },
+        }
+      );
+
+      instance.show();
+    } catch (error) {
+      markupForMistake().show();
+      console.log('Error:', error);
+    }
+  }
+}
+
+function markupForMistake() {
+  const instance = basicLightbox.create(
+    `
+  <div class="trailer-fail">
+  <p class="trailer-fail-text">OOPS...<br/> We are very sorry!<br /> But we couldn’t find the trailer.</p>
+  <button type="button" class="btn-close"><svg class="btn-close--svg">
+  <use href='/sprite.a5e5e87b.svg#icon-close'></use>
+  </svg>
+  </button>
+  <div class="bg-box"></div>
+  </div>
+  `,
+    {
+      handlerEscape: null,
+      handlerBtnClose: null,
+
+      onShow() {
+        this.handlerEscape = handlerEsc.bind(instance);
+        document.addEventListener('keydown', this.handlerEscape);
+
+        const btnCloseEl = instance.element().querySelector('.btn-close');
+
+        this.handlerBtnClose = handlerClose.bind(instance);
+        btnCloseEl.addEventListener('click', this.handlerBtnClose);
+      },
+      onClose() {
+        document.removeEventListener('click', this.handlerBtnClose);
+        document.removeEventListener('keydown', this.handlerEscape);
+      },
+    }
+  );
+
+  return instance;
 }
 
 function handlerEsc(evt) {
@@ -69,72 +150,6 @@ function handlerEsc(evt) {
   }
 }
 
-function handlerClose(evt) {
+function handlerClose() {
   this.close();
 }
-
-// class MovieCardClickHandler {
-//   constructor() {
-//     this.handlerEscape = this.handlerEscape.bind(this);
-//     this.handlerClose = this.handlerClose.bind(this);
-//   }
-
-//   async onMovieCardClick(e) {
-//     e.preventDefault();
-//     if (e.target.offsetParent?.nodeName === 'LI') {
-//       const movieId = Number(e.target.offsetParent.dataset['movie_id']);
-
-//       try {
-//         const movie = await apiMovie.getMovieInfo(movieId);
-
-//         const instance = basicLightbox.create(markupMovieCard(movie.data), {
-//           handlerEscape: null,
-//           handlerBtnClose: null,
-//           onShow: instance => {
-//             const addRemoveBtn = instance
-//               .element()
-//               .querySelector('button[data-type="action"]');
-
-//             const serviceAddRemoveBtn = new ServiceAddRemoveBtn(
-//               addRemoveBtn,
-//               movie
-//             );
-//             serviceAddRemoveBtn.setButtonName();
-
-//             this.handlerEscape = this.handlerEscape.bind(instance);
-//             document.addEventListener('keydown', this.handlerEscape);
-
-//             const btnCloseEl = instance
-//               .element()
-//               .querySelector('#closeModalPopUp');
-
-//             this.handlerBtnClose = this.handlerClose.bind(instance);
-//             btnCloseEl.addEventListener('click', this.handlerBtnClose);
-//           },
-
-//           onClose() {
-//             document.removeEventListener('keydown', this.handlerEscape);
-//             document.removeEventListener('click', this.handlerEscape);
-//           },
-//         });
-
-//         instance.show();
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//   }
-
-//   handlerEscape(evt) {
-//     if (evt.code === 'Escape') {
-//       this.close();
-//     }
-//   }
-
-//   handlerClose(evt) {
-//     this.close();
-//   }
-// }
-
-// const clickHandler = new MovieCardClickHandler();
-// document.addEventListener('click', e => clickHandler.onMovieCardClick(e));
