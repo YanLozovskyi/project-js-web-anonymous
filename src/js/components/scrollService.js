abbc61b1ad9361c8eca6461ae0b560d11696383a
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-14 08:33:26 +0300

    ref ScrollService

--- src/js/components/scrollService.js
+++ src/js/components/scrollService.js
@@ -1,25 +1,36 @@
 export class ScrollService {
+  /**
+   * Блокує прокручування сторінки.
+   */
   blockScroll() {
     document.body.style.overflow = 'hidden';
     document.body.style.paddingRight = this.getWidthScrollbar() + 'px';
   }
 
-  getScroll() {
+  /**
+   * Відновлює прокручування сторінки.
+   */
+  restoreScroll() {
     document.body.style.overflow = '';
     document.body.style.paddingRight = '';
   }
 
+  /**
+   * Отримує ширину полоси прокрутки.
+   * @returns {number} - Ширина полоси прокрутки в пікселях.
+   */
   getWidthScrollbar() {
-    document.body.insertAdjacentHTML(
-      'afterend',
-      `<div id="scrollbar-measure"></div>`
-    );
-    const scrollbarMeasure = document.getElementById('scrollbar-measure');
+    const scrollbarMeasure = document.createElement('div');
     scrollbarMeasure.style.overflow = 'scroll';
+    scrollbarMeasure.style.visibility = 'hidden';
+    scrollbarMeasure.style.position = 'absolute';
+    scrollbarMeasure.style.width = '100px';
+    scrollbarMeasure.style.height = '100px';
+    scrollbarMeasure.style.top = '-9999px';
+    document.body.appendChild(scrollbarMeasure);
     const scrollbarWidth =
       scrollbarMeasure.offsetWidth - scrollbarMeasure.clientWidth;
-    scrollbarMeasure.style.overflow = 'hidden';
-    scrollbarMeasure.remove();
+    document.body.removeChild(scrollbarMeasure);
     return scrollbarWidth;
   }
 }
